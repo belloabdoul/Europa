683df5eb82a84d36b99d72e20509feef0aee192a
bello <belloabdoul@gmail.com>
2024-03-04 11:55:08 -0500

    Removed dependencies injection for IModelService and IAudioService, simplified ffmpeg detection and cleaned up the code

--- API/Features/FindSimilarAudios/Interfaces/IAudioHashGenerator.cs
+++ API/Features/FindSimilarAudios/Interfaces/IAudioHashGenerator.cs
@@ -5,7 +5,6 @@ namespace API.Features.FindSimilarAudios.Interfaces
 {
     public interface IAudioHashGenerator
     {
-        string GenerateHash(string path);
         string GetAudioMatches(string path, IModelService modelService, IAudioService mediaService);
         void GenerateAudioHashes(string path, IModelService modelService, IAudioService mediaService);
     }
