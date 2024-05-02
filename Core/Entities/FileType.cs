e60f3b880f68864524de22df211587b4bbc931bd
bello <belloabdoul@gmail.com>
2024-05-02 09:01:00 -0400

    Renamed Type class to FileType

---
+++ Core/Entities/FileType.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Entities
+{
+    public enum FileType
+    {
+        None,
+        WebpAnimation,
+        Animation,
+        GifImage,
+        Image,
+        Audio,
+        Video,
+        File,
+        Corrupt
+    }
+}
