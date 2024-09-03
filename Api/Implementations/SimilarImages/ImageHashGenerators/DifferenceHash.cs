e11bf4932cbafc2a4429afdd6e5dc4f3aa687e98
Abdoul Koudous Bello <belloabdoul@gmail.com>
2024-09-03 07:54:58 -0400

    Updated DifferenceHash implementation to make used of SIMD

--- Api/Implementations/SimilarImages/ImageHashGenerators/DifferenceHash.cs
+++ Api/Implementations/SimilarImages/ImageHashGenerators/DifferenceHash.cs
@@ -1,4 +1,5 @@
-﻿using Core.Interfaces;
+﻿using System.Runtime.Intrinsics;
+using Core.Interfaces;
 
 namespace Api.Implementations.SimilarImages.ImageHashGenerators;
 
@@ -6,25 +7,25 @@ public class DifferenceHash : IImageHash
 {
     private const int Width = 9;
     private const int Height = 8;
+    private static readonly Vector64<byte> VectorOfZeroes = Vector64.Create<byte>(0);
+    private static readonly Vector64<byte> VectorOfOnes = Vector64.Create<byte>(1);
 
-    public int GetRequiredWidth()
-    {
-        return Width;
-    }
+    public int GetRequiredWidth() => Width;
 
-    public int GetRequiredHeight()
-    {
-        return Height;
-    }
-
+    public int GetRequiredHeight() => Height;
+    
     public byte[] GenerateHash(byte[] pixels)
     {
         var hash = new byte[(Width - 1) * Height];
 
         for (var y = 0; y < Height; y++)
-        for (var x = 0; x < Width - 1; x++)
-            if (pixels[y * Width + x] < pixels[y * Width + x + 1])
-                hash[y * Height + x] = 1;
+        {
+            var firstEight = Vector64.Create<byte>(pixels.AsSpan(y * Width, Width - 1));
+            var lastEight = Vector64.Create<byte>(pixels.AsSpan(y * Width + 1, Width - 1));
+            var condition = Vector64.LessThan(firstEight, lastEight);
+
+            Vector64.ConditionalSelect(condition, VectorOfOnes, VectorOfZeroes).CopyTo(hash.AsSpan(y * Height, Height));
+        }
 
         return hash;
     }
